b44253b0f652c7ece082d688a476bce0b1c0ac78
casparbreloh <caspar.breloh@gmail.com>
2026-01-31 23:47:38 +0100

    feat: change deployment to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import tsconfigPaths from "vite-tsconfig-paths"
 export default defineConfig({
   plugins: [
     devtools(),
-    nitro({ output: { dir: "dist" } }),
+    nitro(),
     tsconfigPaths(),
     tailwindcss(),
     tanstackStart(),
