e7cec57ba845105cba35888dcaddf77884b4429a
casparbreloh <caspar.breloh@gmail.com>
2026-01-31 23:34:09 +0100

    feat: change deployment to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import tsconfigPaths from "vite-tsconfig-paths"
 export default defineConfig({
   plugins: [
     devtools(),
-    nitro({ preset: "aws-amplify", output: { dir: "dist" } }),
+    nitro(),
     tsconfigPaths(),
     tailwindcss(),
     tanstackStart(),
