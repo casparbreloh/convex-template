48f4419048389e2a188ad17a580e6ffb0c8c810b
casparbreloh <caspar.breloh@gmail.com>
2026-02-01 13:51:53 +0100

    feat: change deployment to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
+import netlify from "@netlify/vite-plugin-tanstack-start"
 import tailwindcss from "@tailwindcss/vite"
 import { devtools } from "@tanstack/devtools-vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
-import { nitro } from "nitro/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [
     devtools(),
-    nitro(),
     tsconfigPaths(),
     tailwindcss(),
     tanstackStart(),
+    netlify(),
     viteReact({
       babel: {
         plugins: ["babel-plugin-react-compiler"],
